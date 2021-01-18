d8719fb8b3b756cf44e3012ca4e951a183bbfd76
akitomoj <akitomoj@gmail.com>
2021-01-18 21:48:31 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    # @item_id = Purchase.find_by(item_id: params[:item_id])
     if @item.save
       redirect_to root_path
     else
