9d7b667bfa8bd96e3a728cc7cbc39ba5fbaf62e2
akitomoj <akitomoj@gmail.com>
2021-01-27 08:49:50 +0900

    購入フォーム情報の保存機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,24 @@
 class PurchasesController < ApplicationController
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @purchase = ItemPurchase.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase = ItemPurchase.new(purchase_params)
+    if @purchase.valid?
+
+      @purchase.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
   def purchase_params
-    params.require(:item_purchase).permit(:post_code, :prefecture_id, :city, :address_number, :building, :phone_number, :item_id, :user_id)
+    params.require(:item_purchase).permit(:post_code, :prefecture_id, :city, :address_number, :building, :phone_number, :number, :exp_month, :exp_year, :cvc).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
+# require(:item_purchase).
--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,18 +1,18 @@
 class ItemPurchase
 
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address_number, :building, :phone_number, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :address_number, :building, :phone_number, :item_id, :user_id, :number, :exp_month, :exp_year, :cvc
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :address_number, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Include hyphen(-)"}
   end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(post_code: post_code, prefecture: prefecture, city: city, address_number: address_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address_number: address_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
