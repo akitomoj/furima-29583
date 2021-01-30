5c6f88a2f2f7da0858abc893d08b23cf76008dd5
akitomoj <akitomoj@gmail.com>
2021-01-30 13:00:50 +0900

    rev2-3: item_purchase正規表現編集

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -6,8 +6,8 @@ class ItemPurchase
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :address_number, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :city
+    validates :address_number
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Include hyphen(-)"}
     validates :token
   end
