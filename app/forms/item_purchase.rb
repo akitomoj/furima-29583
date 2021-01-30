feeb66dcd1d171884002d4341f0a827fb4d91f0a
akitomoj <akitomoj@gmail.com>
2021-01-30 13:07:25 +0900

    rev2-4: show.html.erb記述編集

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -6,10 +6,10 @@ class ItemPurchase
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
     validates :address_number
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :token
+    validates :token, :city, :item_id, :user_id
+    
   end
 
   def save
