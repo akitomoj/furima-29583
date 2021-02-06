576cd29d1c14ce9aac75fb51ed724ebf992576ba
akitomoj <akitomoj@gmail.com>
2021-02-06 15:48:09 +0900

    SNS認証導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :items, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :purchases, dependent: :destroy
+  has_many :sns_credentials
 
   with_options presence: true do
     validates :nickname
@@ -23,6 +24,19 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :first_name_katakana
     validates :last_name_katakana
+  end
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email:    auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 end
 
